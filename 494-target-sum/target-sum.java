02139b3d0e56588780b437f0537416ee8998623d
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-03-31 09:43:32 +0530

    Time: 5 ms (79.15%) | Memory: 44.5 MB (50.93%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -5,17 +5,19 @@ class Solution {
         if(Math.abs(target)> totSum || (totSum + target)%2 != 0)return 0;
         int newTar = (totSum + target)/2;
         int n = nums.length;
-        int dp[][] = new int[n+1][newTar+1];
-        dp[0][0] = 1;
+        int prev[] = new int[newTar+1];
+        prev[0] = 1;
         for(int i=1; i<n+1; i++){
+            int curr[] = new int[newTar + 1];
             for(int j=0; j<newTar+1; j++){
                 if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j]; 
+                    curr[j] = prev[j-nums[i-1]] + prev[j]; 
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
-        return dp[n][newTar];
+        return prev[newTar];
     }
 }
