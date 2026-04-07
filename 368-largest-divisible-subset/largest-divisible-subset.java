368fab6b938bb39ad3172cc1673c7a01e0f7a917
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-04-07 22:21:41 +0530

    Time: 17 ms (60.86%) | Memory: 45.4 MB (21.00%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,32 +1,45 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
-        int maxlen = 1;
-        int lastidx = 0;
-        int dp[] =new int[n];
-        int parent[] = new int[n];
-        Arrays.fill(dp,1);
-        for(int i=0; i<n; i++){
+
+        int[] dp = new int[n];
+        int[] parent = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int maxLen = 1;
+        int lastIndex = 0;
+
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
-            for(int prev=0; prev<i; prev++){
-                if(nums[i]%nums[prev]==0 && dp[prev]+1>dp[i]){
+
+            for (int prev = 0; prev < i; prev++) {
+                if (nums[i] % nums[prev] == 0 && dp[prev] + 1 > dp[i]) {
                     dp[i] = dp[prev] + 1;
                     parent[i] = prev;
                 }
             }
-            if(dp[i]>maxlen){
-            maxlen = dp[i];
-            lastidx = i;
+
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIndex = i;
+            }
         }
+
+        // reconstruct subset
+        List<Integer> result = new ArrayList<>();
+
+        while (parent[lastIndex] != lastIndex) {
+            result.add(nums[lastIndex]);
+            lastIndex = parent[lastIndex];
         }
-        ArrayList<Integer> ls = new ArrayList<>();
-        while(parent[lastidx]!=lastidx){
-            ls.add(nums[lastidx]);
-            lastidx = parent[lastidx];
-        }
-        ls.add(nums[lastidx]);
-        Collections.reverse(ls);
-        return ls;
+
+        result.add(nums[lastIndex]);
+
+        Collections.reverse(result);
+        return result;
     }
 }
