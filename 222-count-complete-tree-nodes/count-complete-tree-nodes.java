968edda3379556a9513b29f067bfe4b7217677d9
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-05-01 12:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (92.05%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -16,14 +16,24 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null)return 0;
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+        if(lh==rh)return (int)Math.pow(2,lh)-1;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public static int findLeftHeight(TreeNode root){
         int count = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
+        while(root!=null){
             count++;
-            if(curr.left!=null)q.offer(curr.left);
-            if(curr.right!=null)q.offer(curr.right);
+            root = root.left;
+        }
+        return count;
+    }
+    public static int findRightHeight(TreeNode root){
+        int count = 0;
+        while(root!=null){
+            count++;
+            root = root.right;
         }
         return count;
     }
