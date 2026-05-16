228c4c6d7bb7bec90f769423ae4aa945ce7fafe3
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-05-16 12:43:56 +0530

    Time: 176 ms (25.41%) | Memory: 43 MB (59.98%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,39 +1,63 @@
 class Solution {
+
+    int[] row = {-1,0,1,0};
+    int[] col = {0,1,0,-1};
+
     public boolean exist(char[][] board, String word) {
+
         for(int i=0; i<board.length; i++){
+
             for(int j=0; j<board[0].length; j++){
-                if(find(board, word, i, j, 0)){
+
+                if(board[i][j] == word.charAt(0)
+                    && find(board, word, i, j, 0)){
+
                     return true;
                 }
             }
         }
+
         return false;
     }
-    public boolean find(char[][] board, String word, int i, int j, int idx){
+
+    public boolean find(char[][] board,
+                        String word,
+                        int i,
+                        int j,
+                        int idx){
+
         if(i<0 || i>=board.length ||
            j<0 || j>=board[0].length ||
            board[i][j] != word.charAt(idx)){
 
             return false;
         }
+
         if(idx == word.length()-1){
             return true;
         }
 
         char temp = board[i][j];
         board[i][j] = '#';
-        int row[] = {-1, 0, 1, 0};
-        int col[] = {0, 1, 0, -1};
+
         for(int k=0; k<4; k++){
+
             int nrow = i + row[k];
             int ncol = j + col[k];
-            if(nrow>=0 && nrow<board.length && ncol>=0 && ncol<board[0].length){
-                if(find(board, word, nrow, ncol, idx+1)){
-                    return true;
-                }
+
+            if(find(board,
+                    word,
+                    nrow,
+                    ncol,
+                    idx+1)){
+
+                board[i][j] = temp; // restore before returning
+                return true;
             }
         }
+
         board[i][j] = temp;
+
         return false;
     }
 }
