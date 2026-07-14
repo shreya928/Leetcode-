b96f222d0670cf8b8eac693ac28bf0f9c63b410a
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-07-14 23:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.53%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -9,18 +9,14 @@
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
- *         this.right = right;
+ *         this.right = right;val
  *     }
  * }
  */
 class Solution {
-    public boolean helper(TreeNode p, TreeNode q){
-        if(p==null && q==null){
-            return true;
-        }
-        if(p==null || q==null){
-            return false;
-        }
+    public boolean helper(  TreeNode p, TreeNode q){
+        if(p==null && q==null)return true;
+        if(p==null || q==null)return false;
         if(p.val!=q.val)return false;
         return helper(p.left,q.left) && helper(p.right,q.right);
     }
