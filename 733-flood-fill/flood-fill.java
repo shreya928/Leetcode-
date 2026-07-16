a3b6ba2417909dbcddf2b3f23a3eb0b077cfbd2e
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-07-16 23:40:14 +0530

    Time: 2 ms (12.44%) | Memory: 46.2 MB (98.68%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -2,31 +2,32 @@ class Solution {
     class Pair{
         int row;
         int col;
-        public Pair(int row, int col){
-            this.row = row;
-            this.col = col;
+        public Pair(int r, int c){
+            this.row = r;
+            this.col = c;
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
+        int n = image.length;
+        int m = image[0].length;
         int clr = image[sr][sc];
-        if (clr == color) return image;
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(sr, sc));
+        int vis[][] = new int[n][m];
+        q.offer(new Pair(sr,sc));
         image[sr][sc] = color;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.isEmpty()){
-            Pair pair = q.poll();
-            int row = pair.row;
-            int col = pair.col;
+            Pair p = q.poll();
+            int row = p.row;
+            int col = p.col;
+            vis[row][col] = 1;
             for(int i=0; i<4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]==clr){
-                    image[nrow][ncol] = color;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && image[nrow][ncol]==clr){
                     q.offer(new Pair(nrow,ncol));
+                    image[nrow][ncol] = color;
                 }
             }
         }
