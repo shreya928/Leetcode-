dbc03ac3d8adad745628db239a6781246d2bcdf5
Shreya Pathak <shreyapathak2k4@gmail.com>
2026-07-17 19:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (38.38%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int color[] = new int[graph.length];
-        Arrays.fill(color,-1);
-        for(int i=0; i<graph.length; i++){
-            if(color[i]==-1){
-                if(dfs(i, graph, color, 0)==false)return false;
+       int color[] = new int[graph.length];
+       Arrays.fill(color,-1);
+       for(int i = 0; i < graph.length; i++){
+            if(color[i] == -1){
+                if(!helper(i, graph, color, 0))return false;
             }
         }
         return true;
     }
-    public boolean dfs(int node, int graph[][], int color[], int currCol){
-        color[node] = currCol;
-        for(int neigh :graph[node]){
-            if(color[neigh] == currCol){
+    public boolean helper(int node, int graph[][], int color[], int currClr){
+        color[node] = currClr;
+        for(int neigh : graph[node]){
+            if(color[neigh]==currClr){
                 return false;
-            }
-            if(color[neigh]==-1){
-                int col = (currCol==0) ? 1 : 0;
-                if(dfs(neigh, graph, color, col) == false)return false;
+            }if(color[neigh]==-1){
+                int clr = (currClr == 0 )? 1 : 0;
+                if(!helper(neigh,graph,color,clr))return false;
             }
         }
         return true;
